618c9139a7f32d586264dcb77f223c0749959d89
Ovidiu Maghetiu <ovidiumght@gmail.com>
2017-05-08 21:06:12 +0300

    Initial commit

---
+++ wp-options.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Plugin Name:     Wp Options
+ * Plugin URI:      http://thesolidphp.com
+ * Description:     Simple plugin to exemplify creating a WordPress plugin using Angular 2
+ * Author:          Ovidiu Maghetiu <ovidiumght@gmail.com>
+ * Author URI:      http://thesoildphp.com
+ * Text Domain:     wp-options
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Wp_Options
+ */
